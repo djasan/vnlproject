cbc4aa55ea6aba7099a5422f19a0a1f14767512b
sanson <sansondjamila@orange.fr>
2024-01-18 02:39:30 +0100

    js

--- public/js/script.js
+++ public/js/script.js
@@ -4,12 +4,12 @@
 import { menuEntries } from "./modules/menuEntries.js";
 import { createEntry } from "./modules/menu.js";
 import { burgerManager } from "./modules/burgerManager.js";
-import { slickMin} from "./modules/slider.js";
+import { slider} from "./modules/slider.js";
 
 
 
     const navMenu = document.querySelector("#navMenu");
     createEntry(menuEntries, navMenu);
     burgerManager();
-    Slider();
+    slider();
 
